e67ddc86bcc5f7b9f962c2df2ad2d26f0b251fb2
Pete West <pete.west@torchbox.com>
2025-02-07 18:17:55 +0000

    Add project model with abstract model helpers

---
+++ app/projects/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.constraints import CheckConstraint
+
+from app.utils.models import TimestampedModel, UUIDModel
+
+RETENTION_PERIOD_CHOICES: list[int] = [30, 60, 180]
+
+
+class Project(TimestampedModel, UUIDModel):
+    """
+    Model for a project, which is a grouping for feedback prompts and data collected from a particular domain.
+    """
+
+    name = models.CharField(max_length=128)
+    domain = models.CharField(max_length=256)
+    retention_period_days = models.PositiveSmallIntegerField(
+        choices={
+            choice: f"{choice} days" for choice in RETENTION_PERIOD_CHOICES
+        },
+    )
+    owned_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="+"
+    )
+    created_by = models.ForeignKey(
+        User, on_delete=models.PROTECT, editable=False, related_name="+"
+    )
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                condition=models.Q(
+                    retention_period_days__in=RETENTION_PERIOD_CHOICES
+                ),
+                name="retention_period_days_choices",
+            )
+        ]
+
+    def __str__(self):
+        return self.name
