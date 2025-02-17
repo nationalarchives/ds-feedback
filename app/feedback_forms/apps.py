12ad03ec872ee64079e571d61526d63c5f0d85ef
Pete West <pete.west@torchbox.com>
2025-02-17 14:26:32 +0000

    Add models for FeedbackForm and PathPattern

---
+++ app/feedback_forms/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FeedbackFormConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app.feedback_forms"
+    verbose_name = "FeedbackForm"
