621c9e98ea17fbb601435e2552d587418e995185
Pete West <pete.west@torchbox.com>
2025-02-17 15:54:14 +0000

    Add admin for projects with basic tests

---
+++ app/projects/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from app.projects.models import Project
+from app.utils.admin import (
+    HideReadOnlyOnCreationAdmin,
+    SetCreatedByOnCreationAdmin,
+)
+
+
+class ProjectAdmin(HideReadOnlyOnCreationAdmin, SetCreatedByOnCreationAdmin):
+    ordering = ["-created_at"]
+    readonly_fields = ["uuid", "created_by"]
+    fields = [
+        "uuid",
+        "created_by",
+        "name",
+        "domain",
+        "retention_period_days",
+        "owned_by",
+    ]
+    list_display = [
+        "name",
+        "owned_by",
+        "created_at",
+        "retention_period_days",
+        "uuid",
+    ]
+    list_filter = ["retention_period_days", "owned_by"]
+    search_fields = ["name", "uuid"]
+
+
+admin.site.register(Project, ProjectAdmin)
--- app/utils/testing.py
+++ app/utils/testing.py
@@ -7,3 +7,10 @@ def reverse_with_query(name, query):
     Returns a reversed URL with query parameters
     """
     return reverse(name) + "?" + urlencode(query)
+
+
+def get_change_list_results(response):
+    """
+    Returns the change list results from a response
+    """
+    return list(response.context["cl"].result_list)
