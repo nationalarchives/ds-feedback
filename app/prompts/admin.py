8968d18c2376826ce4c1714cc6a6fb50d8e30ddb
Pete West <pete.west@torchbox.com>
2025-03-13 09:36:59 +0000

    Move PromptAdmin inline to more appropriate app

--- app/prompts/admin.py
+++ app/prompts/admin.py
@@ -1,47 +1,17 @@
-from typing import Type
-
-from django import forms
 from django.contrib import admin
-from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
-from django.urls import reverse
-from django.utils.safestring import mark_safe
 
 from app.prompts.models import (
     BinaryPrompt,
-    Prompt,
     RangedPrompt,
     RangedPromptOption,
     TextPrompt,
 )
 from app.utils.admin import (
-    IsDisabledCheckboxForm,
     IsDisabledHiddenCheckboxForm,
     SetDisabledByWhenDisabledAdmin,
     disallow_duplicates,
 )
-
-ENABLED_PROMPT_LIMIT = 3
-
-PROMPT_LABELS = {
-    TextPrompt.__name__: "Text prompt",
-    BinaryPrompt.__name__: "Binary prompt",
-    RangedPrompt.__name__: "Ranged prompt",
-}
-
-
-PROMPT_VIEWNAMES: dict[Type[Prompt], str] = {
-    TextPrompt: "admin:prompts_textprompt_change",
-    BinaryPrompt: "admin:prompts_binaryprompt_change",
-    RangedPrompt: "admin:prompts_rangedprompt_change",
-}
-
-
-def get_prompt_viewname(prompt):
-    """
-    Gets the viewname for a prompt instance
-    """
-    return PROMPT_VIEWNAMES[type(prompt)]
 
 
 class TextPromptForm(IsDisabledHiddenCheckboxForm):
@@ -230,104 +200,3 @@ class RangedPromptAdmin(SetDisabledByWhenDisabledAdmin):
 admin.site.register(TextPrompt, TextPromptAdmin)
 admin.site.register(BinaryPrompt, BinaryPromptAdmin)
 admin.site.register(RangedPrompt, RangedPromptAdmin)
-
-
-class PromptDetailsWidget(forms.Widget):
-    def render(self, name, value, attrs=None, renderer=None):
-        # self.instance is added by PromptForm
-        if not self.instance.id:
-            return ""
-
-        url = reverse(
-            get_prompt_viewname(self.instance),
-            kwargs={"object_id": self.instance.id},
-        )
-        return mark_safe(f'<a href="{url}">Edit details</a>')
-
-
-class PromptForm(IsDisabledCheckboxForm):
-    prompt_details = forms.CharField(
-        widget=PromptDetailsWidget(),
-        label="prompt details",
-        required=False,
-    )
-    prompt_type = forms.ChoiceField(
-        choices=[
-            ("", "Please select..."),
-            *PROMPT_LABELS.items(),
-        ]
-    )
-
-    class Meta:
-        model = Prompt
-        fields = "__all__"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance:
-            # Pass instance to widget to display link to specific Prompt
-            self.fields["prompt_details"].widget.instance = self.instance
-
-            # For existing prompts set prompt type
-            if self.instance.pk:
-                self.fields["prompt_type"].initial = type(
-                    self.instance
-                ).__name__
-                self.fields["prompt_type"].disabled = True
-                self.fields["prompt_type"].required = False
-
-
-class PromptFormSet(BaseInlineFormSet):
-    def clean(self):
-        super().clean()
-
-        # Check for enabled prompts exceeding limit
-        count = 0
-        for form in self.forms:
-            instance: TextPrompt = form.instance
-
-            if not instance.disabled_at:
-                if instance.text:
-                    count += 1
-
-                if count > ENABLED_PROMPT_LIMIT and instance.order is not None:
-                    form.add_error(
-                        "text",
-                        ValidationError(
-                            f"You cannot have more than {ENABLED_PROMPT_LIMIT} enabled prompts"
-                        ),
-                    )
-
-        disallow_duplicates(
-            self.forms, "order", "This order number is used in another prompt"
-        )
-
-
-class PromptAdmin(admin.TabularInline):
-    model = Prompt
-    form = PromptForm
-    formset = PromptFormSet
-    extra = 1
-    ordering = ["order"]
-    fields = [
-        "text",
-        "prompt_type",
-        "prompt_details",
-        "order",
-        "is_disabled",
-        "disabled_by",
-        "created_by",
-    ]
-    readonly_fields = [
-        "disabled_by",
-        "created_by",
-    ]
-    list_display = ["text", "feedback_form", "order"]
-    list_filter = ["feedback_form"]
-    search_fields = ["text", "uuid"]
-
-    def get_queryset(self, request):
-        query_set = super().get_queryset(request)
-        return query_set.select_subclasses(
-            BinaryPrompt, RangedPrompt, TextPrompt
-        ).select_related("created_by", "disabled_by")
