c98ee91dffe831ce31cd2489e725b9de4413bb15
Ian Meigh <ian.meigh@torchbox.com>
2025-08-04 16:21:02 +0100

    Add page routing and templates

--- app/editor_ui/apps.py
+++ app/editor_ui/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class EditorUiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'editor_ui'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "app.editor_ui"
+    verbose_name = "Editor UI"
