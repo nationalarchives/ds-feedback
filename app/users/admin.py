b387f849aadddea80a2744b6d4ea21fff2225d35
Ian Meigh <ian.meigh@torchbox.com>
2025-08-05 14:56:16 +0100

    Refactor imports

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from app.users.forms import CustomUserChangeForm, CustomUserCreationForm
-
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 
--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from app.users.managers import UserManager
+from .managers import UserManager
 
 
 class User(AbstractUser):
