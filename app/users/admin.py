0622fd2fff008e8958ec091b4f92a2c22e7a5de0
Ian Meigh <ian.meigh@torchbox.com>
2025-08-05 11:41:16 +0100

    Update admin to support email-based user model
    - Use custom user creation/change forms
    - Remove username field from admin
    - Display and filter users by email
    - Adjust permissions and search to use email

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,57 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from app.users.forms import CustomUserChangeForm, CustomUserCreationForm
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password", "first_name", "last_name")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+
+admin.site.register(User, CustomUserAdmin)
---
+++ app/users/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self._create_user(email, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        """
+        Normalize email before lookup so that varying case in the domain
+        doesn’t block authentication.
+        """
+        email = self.normalize_email(username)
+        return self.get(**{self.model.USERNAME_FIELD: email})
