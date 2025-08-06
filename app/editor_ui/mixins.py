0ab1fb388714336d24063b9fe97c98063812d790
Ian Meigh <ian.meigh@torchbox.com>
2025-08-06 14:21:38 +0100

    Add basic view authentication checks

---
+++ app/editor_ui/mixins.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+class AdminPrivRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        return user.is_superuser
