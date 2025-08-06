0c0d4904eb237c404a408d0ee8a94f2ee16d785d
Ian Meigh <ian.meigh@torchbox.com>
2025-08-06 14:20:00 +0100

    Add project creation

---
+++ app/editor_ui/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from app.projects.models import Project
+
+
+shared_text_input_attrs = {
+    "class": "tna-text-input",
+    "type": "text",
+    "spellcheck": "false",
+    "autocapitalize": "off",
+    "autocorrect": "off",
+}
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "name",
+            "domain",
+            "retention_period_days",
+            "owned_by",
+        ]
+        widgets = {
+            "name": forms.TextInput(attrs={**shared_text_input_attrs}),
+            "domain": forms.TextInput(attrs={**shared_text_input_attrs}),
+            "retention_period_days": forms.Select(attrs={"class": "tna-select"}),
+            "owned_by": forms.Select(attrs={"class": "tna-select"}),
+        }
--- app/editor_ui/urls.py
+++ app/editor_ui/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.editor_ui.views import index
+from app.editor_ui.views import ProjectCreateView, index
 
 urlpatterns = [
     path("", index, name="index"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project_create"),
 ]
