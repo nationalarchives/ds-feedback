aa2433abbacc9c6761bf15e9ce6c0b4252f45e9d
Ian Meigh <ian.meigh@torchbox.com>
2025-08-07 14:35:05 +0100

    Add project listing view

--- app/editor_ui/views.py
+++ app/editor_ui/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Q
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from app.editor_ui.forms import ProjectForm
 from app.editor_ui.mixins import SuperuserRequiredMixin
@@ -38,3 +39,46 @@ class ProjectListView(LoginRequiredMixin, ListView):
         else:
             qs = Project.objects.filter(owned_by=self.request.user)
         return qs
+
+
+class ProjectDetailView(LoginRequiredMixin, SuperuserRequiredMixin, DetailView):
+    model = Project
+    template_name = "editor_ui/projects/project_detail.html"
+    slug_field = "uuid"
+    slug_url_kwarg = "uuid"
+
+    def get_queryset(self):
+        return (
+            Project.objects.all()
+            .select_related("owned_by")
+            .annotate(
+                forms_count=Count(
+                    "feedback_forms",
+                    filter=Q(feedback_forms__disabled_at=None),
+                ),
+                responses_count=Count(
+                    "feedback_forms__responses",
+                    filter=Q(feedback_forms__disabled_at=None),
+                ),
+                prompts_count=Count(
+                    "feedback_forms__prompts",
+                    filter=Q(
+                        feedback_forms__disabled_at=None,
+                        feedback_forms__prompts__disabled_at=None,
+                    ),
+                ),
+            )
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.object
+
+        context["forms_count"] = project.forms_count
+        context["responses_count"] = project.responses_count
+        context["prompts_count"] = project.prompts_count
+        context["formatted_created_at"] = project.created_at.strftime(
+            "%d %b %Y"
+        )
+
+        return context
