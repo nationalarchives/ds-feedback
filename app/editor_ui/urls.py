e0dbf4f873958c521f0a345e1c75cb0969258fd9
Ian Meigh <ian.meigh@torchbox.com>
2025-08-07 11:32:33 +0100

    Add project list view

--- app/editor_ui/urls.py
+++ app/editor_ui/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.editor_ui.views import index
+from app.editor_ui.views import ProjectListView, index
 
 urlpatterns = [
     path("", index, name="index"),
+    path("projects/", ProjectListView.as_view(), name="project_list"),
 ]
--- app/editor_ui/views.py
+++ app/editor_ui/views.py
@@ -1,7 +1,25 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from app.projects.models import Project
 
 
 @login_required(login_url="/auth/login/")
 def index(request):
     return render(request, "editor_ui/index.html")
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "editor_ui/projects/project_list.html"
+    context_object_name = "projects"
+
+    def get_queryset(self):
+        # TODO: Project member ship beyond an owner
+        if self.request.user.is_superuser:
+            qs = Project.objects.all()
+        else:
+            qs = Project.objects.filter(owned_by=self.request.user)
+        return qs
