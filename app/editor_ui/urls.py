fd1204dc9dcd7998fb65d69964fae9277790aa34
Ian Meigh <ian.meigh@torchbox.com>
2025-08-07 14:35:05 +0100

    Add project listing view

--- app/editor_ui/urls.py
+++ app/editor_ui/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from app.editor_ui.views import ProjectCreateView, ProjectListView, index
+from app.editor_ui.views import (
+    ProjectCreateView,
+    ProjectDetailView,
+    ProjectListView,
+    index,
+)
 
 urlpatterns = [
     path("", index, name="index"),
     path("projects/", ProjectListView.as_view(), name="project_list"),
     path(
         "projects/create/", ProjectCreateView.as_view(), name="project_create"
+    ),
+    path(
+        "projects/<uuid:uuid>/",
+        ProjectDetailView.as_view(),
+        name="project_detail",
     ),
 ]
