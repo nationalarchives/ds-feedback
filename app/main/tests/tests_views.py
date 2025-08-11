ed095dddb37de311703ea166bfe796315a2ba06f
Ian Meigh <ian.meigh@torchbox.com>
2025-08-11 10:10:00 +0100

    Fix authentication urls

--- app/main/tests/tests_views.py
+++ app/main/tests/tests_views.py
@@ -14,7 +14,7 @@ class TestIndexView(ResetFactorySequencesMixin, TestCase):
 
     def test_get_index_not_authorised(self):
         response = self.client.get(reverse("main:index"))
-        login_url = reverse_with_query("admin:login", {"next": "/"})
+        login_url = reverse_with_query("editor_auth:login", {"next": "/"})
         self.assertRedirects(response, login_url)
 
     def test_get_index_authorised(self):
@@ -29,7 +29,7 @@ class TestAdminIndexView(ResetFactorySequencesMixin, TestCase):
         cls.admin_user = StaffUserFactory()
 
     def test_get_login(self):
-        response = self.client.get(reverse("admin:login"))
+        response = self.client.get(reverse("editor_auth:login"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_get_index_not_authorised(self):
