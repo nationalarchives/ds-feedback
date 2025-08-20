9ad57fd9af5da4f897d84b42c095431ec6bda0f5
Ian Meigh <ian.meigh@torchbox.com>
2025-08-20 15:04:55 +0100

    Fix formatting

--- app/editor_ui/views/project_views.py
+++ app/editor_ui/views/project_views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from django.db.models import (
     Count,
     Q,
