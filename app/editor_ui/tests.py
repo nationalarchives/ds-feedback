89e0815c433720a703631b06a4aee9ffee037750
Ian Meigh <ian.meigh@torchbox.com>
2025-08-21 18:02:04 +0100

    Add test for project resolution logic from url

--- app/editor_ui/tests.py
+++ app/editor_ui/tests.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from django.test import TestCase
 from django.urls import reverse
 
-from app.editor_ui.factories import UserFactory
+from app.editor_ui.factories import ProjectFactory, UserFactory
+from app.editor_ui.mixins import ProjectMembershipRequiredMixin
 from app.projects.models import (
     RETENTION_PERIOD_CHOICES,
     Project,
@@ -131,3 +133,49 @@ class ProjectMembershipTests(TestCase):
             membership_exists,
             "Project creator should have an 'owner' ProjectMembership for the new project",
         )
+
+
+class DummyView(ProjectMembershipRequiredMixin):
+    """
+    Dummy view class for directly testing ProjectMembershipRequiredMixin methods.
+    """
+
+    def __init__(self, kwargs):
+        self.kwargs = kwargs
+
+
+class ProjectMembershipRequiredMixinTests(TestCase):
+    """Tests for the project resolution logic in ProjectMembershipRequiredMixin."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Set up a user and a project for all tests in this class."""
+        cls.creator_user = UserFactory(add_project_creation_permission=True)
+        cls.project = ProjectFactory.create(created_by=cls.creator_user)
+
+    def test_get_project_returns_correct_project_with_valid_uuid(self):
+        """
+        get_project returns the correct Project instance when given a valid UUID.
+        """
+        view = DummyView(kwargs={"project_uuid": self.project.uuid})
+        resolved_project = view.get_project()
+        self.assertEqual(resolved_project, self.project)
+
+    def test_get_project_raises_404_project_test_with_non_matching_uuid(self):
+        """
+        get_project raises Http404 when given a UUID that does not match any project.
+        """
+        non_matching_uuid = "00000000-0000-0000-0000-000000000000"
+        view = DummyView(kwargs={"project_uuid": non_matching_uuid})
+        with self.assertRaises(Http404):
+            view.get_project()
+
+    def test_get_project_raises_if_uuid_missing(self):
+        """
+        get_project raises ImproperlyConfigured if the UUID is missing from the kwargs.
+        """
+        view = DummyView(kwargs={})
+        with self.assertRaisesMessage(
+            Exception, "No project UUID found in URL kwargs"
+        ):
+            view.get_project(kwargs={})
