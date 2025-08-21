46a8c24abc8fe9fb6e4d7517065b34278009598f
Ian Meigh <ian.meigh@torchbox.com>
2025-08-21 12:23:09 +0100

    Fix formatting

--- app/editor_ui/views/project_views.py
+++ app/editor_ui/views/project_views.py
@@ -21,9 +21,6 @@ from app.editor_ui.mixins import (
 )
 from app.editor_ui.views.base_views import BaseCreateView
 from app.projects.models import Project
-from django.db.models import Prefetch
-from django.contrib.auth import get_user_model
-
 from app.users.models import User
 
 
@@ -82,15 +79,17 @@ class ProjectListView(
         }
 
         return qs.filter(**filter_kwargs).distinct()
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         projects = context.get("projects", [])
 
         for project in projects:
-            owners = [str(owner) for owner in getattr(project, "owner_members", [])]
+            owners = [
+                str(owner) for owner in getattr(project, "owner_members", [])
+            ]
             project.owners = ", ".join(owners)
-        
+
         return context
 
     def get_context_data(self, **kwargs):
