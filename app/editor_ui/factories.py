1bcf690d7b36e74d46950692d541492c20243d2c
Ian Meigh <ian.meigh@torchbox.com>
2025-08-21 17:56:14 +0100

    Add project creation permission tests

---
+++ app/editor_ui/factories.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
+
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    email = factory.Faker("email")
+    password = factory.django.Password("changeMe")
+
+    @factory.post_generation
+    def add_project_creation_permission(self, create, extracted, **kwargs):
+        """
+        If 'add_project_permission=True' is passed to the factory, the user will be
+        given the 'projects.add_project' permission.
+        """
+        if not create:
+            return
+        if extracted:
+            perm = Permission.objects.get(codename="add_project")
+            self.user_permissions.add(perm)
