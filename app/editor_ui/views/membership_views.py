0d9e395f0ce7f9ab0c762b8850732c87e2a681eb
Ian Meigh <ian.meigh@torchbox.com>
2025-08-22 13:47:37 +0100

    Restrict project membership editing to owners and admins

--- app/editor_ui/views/membership_views.py
+++ app/editor_ui/views/membership_views.py
@@ -1,3 +1,6 @@
+from typing import Any
+from django.core.exceptions import PermissionDenied
+from django.db.models import BooleanField, Case, When, Value, Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
 )
@@ -15,10 +18,56 @@ class ProjectMembershipListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         project_uuid = self.kwargs.get("project_uuid")
+        user = self.request.user
 
-        return queryset.filter(project__uuid=project_uuid).select_related(
-            "user", "project"
+        return (
+            queryset.filter(project__uuid=project_uuid)
+            .select_related("user", "project")
+            .annotate(
+                is_current_user=Case(
+                    When(user=user, then=Value(True)),
+                    default=Value(False),
+                    output_field=BooleanField(),
+                ),
+            )
         )
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        project_uuid = self.kwargs.get("project_uuid")
+
+        # Determine if the current user is a member of the project
+        current_user_is_member = ProjectMembership.objects.filter(
+            project__uuid=project_uuid, user=user
+        ).exists()
+
+        current_user_is_project_owner = ProjectMembership.objects.filter(
+            project__uuid=project_uuid, user=user, role="owner"
+        ).exists()
+
+        context["current_user_is_project_member_or_admin"] = (
+            current_user_is_member or self.request.user.is_superuser
+        )
+        context["current_user_is_project_owner_or_admin"] = (
+            current_user_is_project_owner or self.request.user.is_superuser
+        )
+
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        project_uuid = self.kwargs.get("project_uuid")
+
+        is_owner = ProjectMembership.objects.filter(
+            project__uuid=project_uuid, user=request.user, role="owner"
+        ).exists()
+
+        # Only allow owners and admins to edit memberships
+        if not is_owner and not self.request.user.is_superuser:
+            raise PermissionDenied(
+                "You do not have permission to edit memberships for this project."
+            )
+        return super().dispatch(request, *args, **kwargs)
 
     # TODO: Safe guard against editing user memberships for projects other than this one
     # if the queryset filter is ever removed by accident and tests fail.
