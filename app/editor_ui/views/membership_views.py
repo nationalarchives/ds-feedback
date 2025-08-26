a7f10519fcb34bac7c8a9538ddf55ec1a04c544c
Ian Meigh <ian.meigh@torchbox.com>
2025-08-26 12:48:24 +0100

    Add permissions checks

--- app/editor_ui/views/membership_views.py
+++ app/editor_ui/views/membership_views.py
@@ -1,19 +1,30 @@
-from typing import Any
-from django.core.exceptions import PermissionDenied
-from django.db.models import BooleanField, Case, When, Value, Q
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
 )
+from django.core.exceptions import PermissionDenied
+from django.db.models import BooleanField, Case, Value, When
 from django.views.generic import ListView
 
-from app.projects.models import ProjectMembership
+from app.editor_ui.mixins import (
+    ProjectMembershipRequiredMixin,
+)
+from app.projects.models import Project, ProjectMembership
 
 
-class ProjectMembershipListView(LoginRequiredMixin, ListView):
+class ProjectMembershipListView(
+    LoginRequiredMixin,
+    ProjectMembershipRequiredMixin,
+    ListView,
+):
     model = ProjectMembership
     template_name = "editor_ui/projects/project_membership_list.html"
     context_object_name = "members"
     required_project_roles = ["editor", "owner"]
+
+    # ProjectMembershipRequiredMixin mixin attributes
+    project_roles_required = ["editor", "owner"]
+    parent_model = Project
+    parent_lookup_kwarg = "project_uuid"
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -32,7 +43,7 @@ class ProjectMembershipListView(LoginRequiredMixin, ListView):
             )
         )
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         project_uuid = self.kwargs.get("project_uuid")
@@ -46,11 +57,14 @@ class ProjectMembershipListView(LoginRequiredMixin, ListView):
             project__uuid=project_uuid, user=user, role="owner"
         ).exists()
 
-        context["current_user_is_project_member_or_admin"] = (
-            current_user_is_member or self.request.user.is_superuser
-        )
-        context["current_user_is_project_owner_or_admin"] = (
-            current_user_is_project_owner or self.request.user.is_superuser
+        context.update(
+            {
+                "current_user_is_project_member_or_admin": current_user_is_member
+                or self.request.user.is_superuser,
+                "current_user_is_project_owner_or_admin": current_user_is_project_owner
+                or self.request.user.is_superuser,
+                "project_uuid": project_uuid,
+            }
         )
 
         return context
@@ -58,19 +72,17 @@ class ProjectMembershipListView(LoginRequiredMixin, ListView):
     def dispatch(self, request, *args, **kwargs):
         project_uuid = self.kwargs.get("project_uuid")
 
-        is_owner = ProjectMembership.objects.filter(
-            project__uuid=project_uuid, user=request.user, role="owner"
-        ).exists()
+        # Get the user's roles for the project
+        roles = ProjectMembership.objects.filter(
+            project__uuid=project_uuid, user=request.user
+        ).values_list("role", flat=True)
+        is_owner = "owner" in roles
+        is_editor = "editor" in roles
 
-        # Only allow owners and admins to edit memberships
-        if not is_owner and not self.request.user.is_superuser:
-            raise PermissionDenied(
-                "You do not have permission to edit memberships for this project."
-            )
+        # Ensure only project members can edit membership listing
+        if not self.request.user.is_superuser:
+            if not is_owner or not is_editor:
+                raise PermissionDenied(
+                    "You do not have permission to edit memberships for this project."
+                )
         return super().dispatch(request, *args, **kwargs)
-
-    # TODO: Safe guard against editing user memberships for projects other than this one
-    # if the queryset filter is ever removed by accident and tests fail.
-
-    # The edit button won't be displayed unless users are owners of the project.
-    # The add and detail view needs to be restricted to owners of the project as well.
