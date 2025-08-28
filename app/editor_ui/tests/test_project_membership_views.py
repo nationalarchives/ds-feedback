e55ddb15bd47bb79b2ebfefcaeb3e9973058e328
Ian Meigh <ian.meigh@torchbox.com>
2025-08-28 12:44:49 +0100

    test: add membership deletion tests

--- app/editor_ui/tests/test_project_membership_views.py
+++ app/editor_ui/tests/test_project_membership_views.py
@@ -72,12 +72,84 @@ class ProjectListViewTests(TestCase):
             .replace("  ", " ")
         )
         self.assertRegex(content, r'<a [^>]*class="tna-button"[^>]*>Delete</a>')
-        self.assertNotRegex(content, r'<a [^>]*class="tna-button"[^>]*>Edit</a>')
+        self.assertNotRegex(
+            content, r'<a [^>]*class="tna-button"[^>]*>Edit</a>'
+        )
 
     def test_editor_with_project_membership_cannot_add_users(self):
         self.client.force_login(self.editor)
         response = self.client.get(
-            reverse("editor_ui:project_memberships_add", args=[self.project.uuid])
+            reverse(
+                "editor_ui:project_memberships_add", args=[self.project.uuid]
+            )
         )
 
         self.assertEqual(response.status_code, 403)
+
+    def test_editor_can_remove_self(self):
+        self.client.force_login(self.editor)
+        editor_membership = ProjectMembership.objects.get(
+            project=self.project, user=self.editor
+        )
+        response = self.client.post(
+            reverse(
+                "editor_ui:project_memberships_delete",
+                args=[self.project.uuid, editor_membership.uuid],
+            ),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse("editor_ui:project_list"))
+        self.assertFalse(
+            ProjectMembership.objects.filter(
+                project=self.project, user=self.editor
+            ).exists()
+        )
+
+    def test_editor_cannot_remove_others(self):
+        self.client.force_login(self.editor)
+        owner_membership = ProjectMembership.objects.get(
+            project=self.project, user=self.owner
+        )
+        response = self.client.post(
+            reverse(
+                "editor_ui:project_memberships_delete",
+                args=[self.project.uuid, owner_membership.uuid],
+            ),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(
+            ProjectMembership.objects.filter(
+                project=self.project, user=self.owner, role="owner"
+            ).exists()
+        )
+
+    def test_cannot_delete_last_owner(self):
+        self.client.force_login(self.owner)
+        owner_membership = ProjectMembership.objects.get(
+            project=self.project, user=self.owner
+        )
+        response = self.client.post(
+            reverse(
+                "editor_ui:project_memberships_delete",
+                args=[self.project.uuid, owner_membership.uuid],
+            ),
+            follow=True,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(
+            response,
+            reverse("editor_ui:project_memberships", args=[self.project.uuid]),
+        )
+        self.assertContains(
+            response, "Each project must have at least one owner."
+        )
+        self.assertTrue(
+            ProjectMembership.objects.filter(
+                project=self.project, user=self.owner, role="owner"
+            ).exists()
+        )
