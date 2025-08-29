192ebdc1b83bd0cd6613a0803d6bedbb58f3db76
Madhav Manoj <madhav.manoj@torchbox.com>
2025-08-29 17:19:17 +0100

    feat: add api key create screen

--- app/editor_ui/urls/api_key_urls.py
+++ app/editor_ui/urls/api_key_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from app.editor_ui.views.api_key_views import ApiKeyDetailView
+from app.editor_ui.views.api_key_views import ApiKeyListView, ApiKeyCreateView
 
 
 app_name = "api_keys"
 
 urlpatterns = [
-    path("", ApiKeyDetailView.as_view(), name="list"),
+    path("", ApiKeyListView.as_view(), name="list"),
+    path("create/", ApiKeyCreateView.as_view(), name="create"),
 ]
--- app/editor_ui/views/api_key_views.py
+++ app/editor_ui/views/api_key_views.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
 from rest_framework.authtoken.models import Token
 
 from app.editor_ui.mixins import BreadCrumbsMixin
 
 
-class ApiKeyDetailView(LoginRequiredMixin, BreadCrumbsMixin, ListView):
+class ApiKeyListView(LoginRequiredMixin, BreadCrumbsMixin, ListView):
     """
-    A view to list the API keys associated with the user.
+    A view to show the API key associated with the user.
     API keys are represented in the Token model from django rest framework.
     """
 
@@ -23,3 +25,31 @@ class ApiKeyDetailView(LoginRequiredMixin, BreadCrumbsMixin, ListView):
         Get only the Token that the user has access to
         """
         return super().get_queryset().filter(user=self.request.user)
+
+
+class ApiKeyCreateView(LoginRequiredMixin, BreadCrumbsMixin, CreateView):
+    """
+    A view to confirm whether the user would like to create a API Key for their use.
+    """
+
+    model = Token
+    fields = []
+    template_name = "editor_ui/user/api_key_create.html"
+    success_url = reverse_lazy("editor_ui:api_key_list")
+
+    breadcrumb = "Create API Key"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["has_existing_token"] = Token.objects.filter(
+            user=self.request.user
+        ).exists()
+        return context
+
+    def form_valid(self, form):
+        existing_token = Token.objects.filter(user=self.request.user).first()
+        if existing_token:
+            existing_token.delete()
+
+        self.object = Token.objects.create(user=self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
