4d125b39451051aa581a51c1a9af24d1a624616f
Madhav Manoj <madhav.manoj@torchbox.com>
2025-09-14 22:43:10 +0100

    fix: formatting and import order

--- app/editor_ui/views/api_access_views.py
+++ app/editor_ui/views/api_access_views.py
@@ -1,10 +1,10 @@
-from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.db.models import F
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
-from django.views.generic import ListView, DeleteView
+from django.views.generic import DeleteView, ListView
 
 from app.api.models import ProjectAPIAccess
 from app.api.types import APIRole
