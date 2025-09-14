429b1782eba3155874f8b25dc56eeacecccc90c4
Madhav Manoj <madhav.manoj@torchbox.com>
2025-09-14 17:59:33 +0100

    fix: pick up db from url if available

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,5 +1,7 @@
 import os
 
+import dj_database_url
+
 from .base import *
 from .base import BASE_DIR, INSTALLED_APPS
 from .features import *
@@ -15,7 +17,8 @@ SECRET_KEY = "abc123"
 DEBUG = True
 
 DATABASES = {
-    "default": {
+    "default": dj_database_url.config(conn_max_age=600)
+    or {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
