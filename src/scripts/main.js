3b55afca71bdb737ac0a98b96771f033ba54dd9c
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-09-15 17:21:18 +0100

    Update TNA Frontend

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,5 @@
 import {
-  Cookies,
-  initAll,
+  initAll
 } from "@nationalarchives/frontend/nationalarchives/all.mjs";
-
-const cookiesDomain =
-  document.documentElement.getAttribute("data-cookiesdomain");
-if (cookiesDomain) {
-  new Cookies({ domain: cookiesDomain });
-}
 
 initAll();
